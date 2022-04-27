2dbaac170a951d1828bc2b8e55219b200672baae
lisohh <sohyunlee0902@gmail.com>
2022-04-28 00:09:27 +0900

    Add: 1.4CSS Modules 예시2

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,12 +5,14 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link href="/">
-        <a className="hello">Home</a>
+        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a className={router.pathname === "/about" ? styles.active : ""}>
+          About
+        </a>
       </Link>
     </nav>
   );
