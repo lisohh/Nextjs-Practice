c0cdbe9f2846f3f3fa9ca2e9655c0a29089b45fc
lisohh <sohyunlee0902@gmail.com>
2022-04-28 00:13:51 +0900

    Add: 1.4 CSS Modules 예시3

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,10 +7,21 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a className={router.pathname === "/about" ? styles.active : ""}>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
           About
         </a>
       </Link>
