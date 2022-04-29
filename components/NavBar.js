2dc9943ab6b44ea488c46f28f6e98b5fbcc70d37
lisohh <sohyunlee0902@gmail.com>
2022-04-29 16:16:24 +0900

    Add: 1.5 styled Jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,26 @@
 import Link from "next/Link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a>About</a>
       </Link>
+      <style jsx>
+        {`
+          nav {
+            background-color: tomato;
+          }
+          a {
+            text-decoration: none;
+          }
+        `}
+      </style>
     </nav>
   );
 }
